2d6862bc1ac1f24379e5613201a34edf3f103e90
deatil <2217957370@qq.com>
2021-02-02 13:06:56 +0800

    更新

--- src/addon/luser/info.php
+++ src/addon/luser/info.php
@@ -9,7 +9,6 @@ return array(
     'authoremail' => 'deatil@github.com',
     'version' => '1.0.3',
     'adaptation' => '2.0.2',
-    'sign' => '7a234fc129f2046ffc33dbfa4d153c91',
     
     // 模块地址
     'path' => __DIR__,
@@ -19,11 +18,4 @@ return array(
     'event' => [],
     
     'menus' => include __DIR__ . '/menu.php',
-    
-    'tables' => [
-        'luser_config',
-        'luser_user',
-        'luser_access',
-        'luser_log',
-    ],
 );
